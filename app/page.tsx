f423531f2487d47793979044567bd2e8ff057827
dieptv <diep.tv1999@gmail.com>
2023-03-12 23:12:10 +0700

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen h-full w-full flex-col space-y-20">
-      <div className="text-4xl uppercase">Trò chơi ô chữ</div>
+      <div className="text-5xl uppercase text-white">Trò chơi ô chữ</div>
       <div className="flex flex-col items-center space-y-16">
         <div className="flex flex-row space-x-12">
-          <a className="px-8 py-6 shadow-lg rounded-lg text-3xl" href={'/kv1/topic-1'}>KV1</a>
-          <a className="px-8 py-6 shadow-lg rounded-lg text-3xl" href={'/kv2/topic-1'}>KV2</a>
-          <a className="px-8 py-6 shadow-lg rounded-lg text-3xl" href={'/kv3/topic-1'}>KV3</a>
+          <a className="px-8 py-6 shadow-lg rounded-lg text-3xl bg-[#e5e7eb]" href={'/kv1/topic-1'}>KV1</a>
+          <a className="px-8 py-6 shadow-lg rounded-lg text-3xl bg-[#e5e7eb]" href={'/kv2/topic-1'}>KV2</a>
+          <a className="px-8 py-6 shadow-lg rounded-lg text-3xl bg-[#e5e7eb]" href={'/kv3/topic-1'}>KV3</a>
         </div>
         {/*<a className="px-8 py-6 shadow-lg rounded-lg text-3xl" href={'/final/topic-1'}>CHUNG KẾT</a>*/}
       </div>
